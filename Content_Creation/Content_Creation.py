55ba8832e6155afd0958416818742b838f7772cc
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-05-13 10:39:09 +0530

    Update Content_Creation.py

--- Content_Creation/Content_Creation.py
+++ Content_Creation/Content_Creation.py
@@ -1,8 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pathlib
+import base64
+#import plotly.express as px
+#from plotly.subplots import make_subplots
+#import plotly.graph_objects as go
+#import matplotlib.pyplot as plt
+
 import requests
 import urllib
 import pandas as pd
 from requests_html import HTML
 from requests_html import HTMLSession
+
+st.title("Content Creation for the Given Topic using Web Scraping and NLP")
+st.sidebar.title("Content Creation for the Given Topic using Web Scraping and NLP")
+#st.markdown("This application is to extract URLs and text content related to the given topic:")
+st.markdown("***")
+st.sidebar.markdown("This application is to extract URLs and text content for the given topic")
 
 def get_source(url):
     """Return the source code for the provided URL. 
@@ -35,14 +51,7 @@ def scrape_google(query):
                       'https://policies.google.',
                       'https://support.google.',
                       'https://maps.google.',
-                      'https://www.youtube.',
-                     'https://www.edureka.',
-                     'http://www.edureka.',
-                     'https://www.coursera.',
-                     'http://www.coursera.',
-                     'https://www.udacity.',
-                     'https://www.datacamp.',
-                     'https://www.udemy.')
+                      'https://www.youtube.')
 
     for url in links[:]:
         if url.startswith(google_domains):
@@ -50,78 +59,43 @@ def scrape_google(query):
 
     return links
 
-#word = "tutorials"
-Topic = input("Please input the topic here: ")
-df1 = pd.DataFrame(scrape_google(Topic), columns = ['link'])
-#df1 = pd.DataFrame(scrape_google(Topic + ' ' + word), columns = ['link'])
-df = df1[~df1['link'].str.contains('cours', na=False)]
-#for x in df['link']:
-#    print(x)
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
 
-#def get_results(query):
-#    
-#    query = urllib.parse.quote_plus(query)
-#    response = get_source("https://www.google.co.in/search?q=" + query)
-#    
-#    return response
-#
-#def parse_results(response):
-#    
-#    css_identifier_result = ".tF2Cxc"
-#    css_identifier_title = "h3"
-#    css_identifier_link = ".yuRUbf a"
-#    css_identifier_text = ".IsZvec"
-#    
-#    results = response.html.find(css_identifier_result)
-#
-#    output = []
-#    
-#    for result in results:
-#
-#        item = {
-#            'title': result.find(css_identifier_title, first=True).text,
-#            'link': result.find(css_identifier_link, first=True).attrs['href'],
-#            'text': result.find(css_identifier_text, first=True).text
-#        }
-#        
-#        output.append(item)
-#        
-#    return output
-#
-#def google_search(query):
-#    response = get_results(query)
-#    return parse_results(response)
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
 
-#Topic = input("Please input the topic here: ")
-#results = google_search(Topic)
-#results = google_search(Topic + " " + word)
-#results1 = list(results)
-#df = pd.DataFrame(results1)
-# print(df)
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
 
-list2 = []
-import trafilatura
-for url in df['link']:
-    downloaded = trafilatura.fetch_url(url)
-    trafilatura.extract(downloaded)
-    # outputs main content and comments as plain text ...
-    list1 = trafilatura.extract(downloaded, include_comments=False)
-    # outputs main content without comments as XML ...
-    list2.append(list1)
-    list3 = ''.join(filter(None, list2))
-#    print(list3)
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
 
-import re
-text2 = re.compile('[.!?] ').split(list3)
-print("\n No. of extracted sentences: ")
-print(len(text2))
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode("UTF-8")).decode()
 
-print("\n \n Below are the scraped urls:")
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
-for x in df['link']:
-    print("\n", x)
 
-print("\n Below are the extracted texts from the above urls: ")
-
-for x in text2:
-    print("\n", x)
+Topic = st.text_input('Input the topic here:')
+def Extract_urls():
+    if len(Topic)>0:
+        if st.button("Extract URLs"):
+    #    if text is not None:
+            with st.spinner("Extracting..."):
+                df = pd.DataFrame(scrape_google(Topic), columns = ['link'])
+                #df1 = pd.DataFrame(scrape_google(Topic + ' ' + word), columns = ['link'])
+#                df = df1[~df1['link'].str.contains('cours', na=False)]
+                if st.button('Download Output as a text file'):
+                    tmp_download_link = download_link(df, 'Your_Output.txt', 'Click here to download your output!')
+                    st.markdown(tmp_download_link, unsafe_allow_html=True)
+                st.write("Below are the top URLs to extract content:")
+                for x in df['link']:
+                    st.write(x)
+#    else:
+#        st.error("Please input the topic again")
+Extract_urls()
