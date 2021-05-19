b4ec18885a48f434965ad757b23d9b673876c7a2
DeepsphereAI <DeepSphereAIInfo@gmail.com>
2021-05-19 10:16:56 +0530

    commit

---
+++ 26-Chatbot/streamlitchatbot.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def chatbot():
+    df = pd.read_excel("faq.xlsx")
+    df.dropna(inplace=True)
+    vectorizer = TfidfVectorizer()   
+    vectorizer.fit(np.concatenate((df.Question, df.Answer)))
+    Question_vectors = vectorizer.transform(df.Question)
+    st.write("You can start chatting with me now.")
+    input_question = get_text()
+    if True:
+        # Read user input
+   
+        # Locate the closest question
+        input_question_vector = vectorizer.transform([input_question])
+
+        # Compute similarities
+        similarities = cosine_similarity(input_question_vector, Question_vectors)
+
+        # Find the closest question
+        closest = np.argmax(similarities, axis=1)
+
+        # Print the correct answer
+        st.text_area("BOT: " + df.Answer.iloc[closest].values[0])
+        
+def get_text():
+    input_text = st.text_input("You: ","So, what's in your mind")
+    return input_text
+
+def main():
+
+    if st.button("lets talk"):
+        chatbot()
+
+if __name__ == '__main__':
+    main()
