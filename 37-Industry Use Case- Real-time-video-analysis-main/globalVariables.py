55bfc81a8285dd44cfdfcf647815c8f81669c0fb
kanika158 <kanikajindalggc@gmail.com>
2021-08-12 11:56:52 +0530

    commit

---
+++ 37-Industry Use Case- Real-time-video-analysis-main/globalVariables.py
@@ -0,0 +1,21 @@
+
+# Video Parameters
+GLOBAL_ROW = 256
+# Scale according to the size of the row
+GLOBAL_COL = 256
+GLOBAL_FPS = 20
+
+# Spark Configuration
+GLOBAL_BATCH_DURATION = 2
+GLOBAL_WINDOW_DURATION = 10
+GLOBAL_SLIDE_DURATION = 10
+GLOBAL_BROKER = "localhost:9092"
+
+# Live Video Address
+# GLOBAL_STREAM_ADDRESS = 'rtmp://aistranger.net:1935/hls'
+GLOBAL_STREAM_ADDRESS = 'rtmp://media3.scctv.net/live/scctv_800'
+
+# Third part API
+URL = "https://api-us.faceplusplus.com/facepp/v3/detect"
+API_KEY = "vjNZBuJIPB6u5jn9SXRSsMczEuVe_oct"
+API_SECRET = "6xEDy9wURWJq78AB5LLOR4Rj54jgGZ4O"
